6c22b78d4dce92add78c97c1e5ed6874338b248f
krkarma777 <krkarma777@gmail.com>
2024-01-28 14:48:40 +0900

    initial commit

---
+++ src/main/java/com/bulkpurchase/BulkPurchaseSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.bulkpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkPurchaseSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulkPurchaseSubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bulkpurchase/config/UserMapper.java
@@ -0,0 +1,10 @@
+package com.bulkpurchase.config;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+
+}
