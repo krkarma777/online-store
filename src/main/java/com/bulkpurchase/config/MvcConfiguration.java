6b62c85391fb59a794e9c1c05ee0e125d37ab6da
krkarma777 <krkarma777@gmail.com>
2024-02-06 16:26:57 +0900

    feat : 정적 리소스 로딩을 위한 설정 클래스 추가

---
+++ src/main/java/com/bulkpurchase/config/MvcConfiguration.java
@@ -0,0 +1,24 @@
+package com.bulkpurchase.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/font/**'로 호출하는 자원은 '/static/font/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60 * 60 * 24 * 365);
+    }
+
+}
