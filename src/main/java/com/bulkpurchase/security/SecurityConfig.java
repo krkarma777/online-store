c9b0aa44e8ae0c26777a6ef29a4562156af1eb97
krkarma777 <krkarma777@gmail.com>
2024-02-14 11:37:01 +0900

    feat : OAuth2를 통한 kakao, naver 로그인 구현

--- src/main/java/com/bulkpurchase/security/SecurityConfig.java
+++ src/main/java/com/bulkpurchase/security/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.bulkpurchase.security;
 
 import com.bulkpurchase.security.handler.LoginAuthenticationFailureHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configurable
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final DefaultOAuth2UserService defaultOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -18,6 +25,7 @@ public class SecurityConfig {
         httpSecurity
                 .authorizeHttpRequests(
                         (authorize) -> authorize
+                                .requestMatchers("/","/api/v1/auth/**","/oauth2/**").permitAll()
                                 .requestMatchers("/css/**", "/js/**", "/img/**").permitAll() // 정적 리소스 허용
                                 .requestMatchers("/register/**", "/registerProc", "/login/**", "/loginProc", "/").permitAll()
                                 .requestMatchers("/mypage/**", "/cart/**").authenticated()
@@ -26,6 +34,11 @@ public class SecurityConfig {
 /*                                .requestMatchers("/*").permitAll()*/
                                 .anyRequest().permitAll()
                 )
+
+/*                .oauth2Login(oauth2 -> oauth2
+                        .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
+                        .userInfoEndpoint(endpoint -> endpoint.userService(defaultOAuth2UserService))
+                )*/
 
                 .formLogin((authorize) -> authorize
                         .loginPage("/login")
@@ -43,7 +56,11 @@ public class SecurityConfig {
                                 .permitAll())
 
                 .csrf(AbstractHttpConfigurer::disable
-                );
+                )
+
+
+
+        ;
         return httpSecurity.build();
     }
 
