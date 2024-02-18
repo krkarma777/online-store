fe6ec68c3f3dedd28d18c7722044d15185ee4bda
krkarma777 <krkarma777@gmail.com>
2024-02-18 16:48:27 +0900

    feat : 개별 주문상품의 배송정보에 따른 전체 주문 상태 변경 로직 구현

--- src/main/java/com/bulkpurchase/web/controller/seller/OrderManageController.java
+++ src/main/java/com/bulkpurchase/web/controller/seller/OrderManageController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -28,19 +29,43 @@ public class OrderManageController {
                                     @RequestParam(value = "orderStatus") OrderStatus status) {
         User user = userService.findByUsername(principal.getName());
         Optional<OrderDetail> orderDetailOpt = orderDetailService.findByID(orderDetailID);
+
         if (orderDetailOpt.isPresent()) {
             OrderDetail orderDetail = orderDetailOpt.get();
             if (orderDetail.getProduct().getUser().equals(user)) {
                 orderDetail.setStatus(status);
                 orderDetailService.save(orderDetail);
+
+                Order order = orderDetail.getOrder();
+                List<OrderDetail> details = orderDetailService.findByOrder(order);
+                // 주문 상태 업데이트 로직 시작
+                boolean allDelivered = details.stream().allMatch(d -> d.getStatus() == OrderStatus.DELIVERED);
+                boolean anyCancelled = details.stream().anyMatch(d -> d.getStatus() == OrderStatus.CANCELLED);
+                boolean anyShipped = details.stream().anyMatch(d -> d.getStatus() == OrderStatus.SHIPPED);
+                boolean allProcessingOrShipped = details.stream().allMatch(d ->
+                        d.getStatus() == OrderStatus.PROCESSING || d.getStatus() == OrderStatus.SHIPPED);
+
+                if (anyCancelled) {
+                    order.setStatus(OrderStatus.CANCELLED);
+                } else if (allDelivered) {
+                    order.setStatus(OrderStatus.DELIVERED);
+                } else if (anyShipped && allProcessingOrShipped) {
+                    order.setStatus(OrderStatus.SHIPPED);
+                } else {
+                    order.setStatus(OrderStatus.PROCESSING);
+                }
+                // 주문 상태 업데이트 로직 종료
+
+                orderService.save(order); // 주문 상태 저장
                 return "redirect:/seller/orders";
             } else {
                 // 셀러가 그 주문에 접근할 권한이 없음
                 return "error/404";
             }
         } else {
-            //존재하지 않는 주문
+            // 존재하지 않는 주문
             return "error/400";
         }
     }
+
 }
