c560f81ae0007f5adae131cfe780c2fb6ef7dbf6
krkarma777 <krkarma777@gmail.com>
2024-02-29 00:46:56 +0900

    feat : paypal 결제 목업 구현

---
+++ src/main/java/com/bulkpurchase/web/controller/payment/PaymentController.java
@@ -0,0 +1,19 @@
+package com.bulkpurchase.web.controller.payment;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaymentController {
+
+    @Value("${paypal.client.id}")
+    private String paypalClientId;
+
+    @GetMapping("/paypal-test")
+    public String paypalTest(Model model) {
+        model.addAttribute("paypalClientId", paypalClientId);
+        return "paymentGateway/paypal-test";
+    }
+}
