f9b3f0e86f1fab9f587732292af1dcfd8195c739
krkarma777 <krkarma777@gmail.com>
2024-03-02 22:54:39 +0900

    chore : discountpolicy 어노테이션 변경

--- src/main/java/com/bulkpurchase/web/policy/discount/GlobalPercentageDiscountPolicy.java
+++ src/main/java/com/bulkpurchase/web/policy/discount/GlobalPercentageDiscountPolicy.java
@@ -1,9 +1,10 @@
 package com.bulkpurchase.web.policy.discount;
 
 import com.bulkpurchase.domain.entity.discount.GlobalDiscount;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class GlobalPercentageDiscountPolicy implements GlobalDiscountPolicy {
     public Double discount(GlobalDiscount globalDiscount, Double totalPrice) {
         double applyPrice = totalPrice * (1 - globalDiscount.getDiscount() / 100);
