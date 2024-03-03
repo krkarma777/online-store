6998f7ca2bb925a43494342b4e3edaa706634b5c
krkarma777 <krkarma777@gmail.com>
2024-03-03 11:28:24 +0900

    feat: 전역 할인 정책 수정 기능 추가
    전역 할인 정책을 관리하는 관리자 페이지에 할인 정책 수정 기능을 추가함. 사용자는 이제 할인 정책 목록에서 특정 할인 정책을 선택하여 수정할 수 있으며, 수정된 정보는 전역 할인 정책 서비스를 통해 데이터베이스에 저장됨. 수정 기능은 jQuery와 AJAX를 사용하여 구현되었으며, 사용자 경험을 개선하기 위해 부트스트랩 모달을 활용함.

--- src/main/java/com/bulkpurchase/domain/service/GlobalDiscountService.java
+++ src/main/java/com/bulkpurchase/domain/service/GlobalDiscountService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +21,9 @@ public class GlobalDiscountService {
 
     public List<GlobalDiscount> findAll() {
         return globalDiscountRepository.findAll(Sort.by(Sort.Direction.ASC, "globalDiscountID"));
+    }
+
+    public Optional<GlobalDiscount> findById(Long discountId) {
+        return globalDiscountRepository.findById(discountId);
     }
 }
--- src/main/java/com/bulkpurchase/web/controller/admin/AdminDiscountPolicyManageController.java
+++ src/main/java/com/bulkpurchase/web/controller/admin/AdminDiscountPolicyManageController.java
@@ -3,10 +3,14 @@ package com.bulkpurchase.web.controller.admin;
 import com.bulkpurchase.domain.entity.discount.GlobalDiscount;
 import com.bulkpurchase.domain.service.GlobalDiscountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -28,5 +32,16 @@ public class AdminDiscountPolicyManageController {
     public String createDiscountPolicy(GlobalDiscount globalDiscount) {
         globalDiscountService.save(globalDiscount);
         return "redirect:/admin/event/discount";
+    }
+    @PostMapping("/admin/event/discount/update")
+    public String updateDiscountPolicy(GlobalDiscount globalDiscount) {
+        globalDiscountService.save(globalDiscount);
+        return "redirect:/admin/event/discount";
+    }
+    @GetMapping("/discount/{discountId}")
+    @ResponseBody
+    public GlobalDiscount updateDiscountPolicyForm(@PathVariable("discountId") Long discountId) {
+        return globalDiscountService.findById(discountId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "할인 정책을 찾을 수 없습니다."));
     }
 }
