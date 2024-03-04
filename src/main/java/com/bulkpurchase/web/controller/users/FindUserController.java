51a4e9678a285ce9a479952eb57b55927bd5478d
krkarma777 <krkarma777@gmail.com>
2024-03-04 11:27:26 +0900

    feat : 회원 아이디 찾기 이메일 발송 시스템 추가

--- src/main/java/com/bulkpurchase/web/controller/users/FindUserController.java
+++ src/main/java/com/bulkpurchase/web/controller/users/FindUserController.java
@@ -2,6 +2,8 @@ package com.bulkpurchase.web.controller.users;
 
 import com.bulkpurchase.domain.entity.user.User;
 import com.bulkpurchase.domain.service.user.UserService;
+import com.bulkpurchase.web.service.verification.EmailService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -17,23 +19,24 @@ import java.util.Optional;
 public class FindUserController {
 
     private final UserService userService;
+    private final EmailService emailService;
 
-    @GetMapping("/find/user")
+    @GetMapping("/find/id")
     public String findUserByEmailForm() {
         return "users/findUserByEmail";
     }
 
     @PostMapping("/find/id")
     public String findUserByEmail(@RequestParam("name") String realName,
-                                  @RequestParam("email") String email) {
+                                  @RequestParam("email") String email) throws MessagingException {
         Optional<User> userOpt = userService.findByRealNameAndEmail(realName, email);
         if (userOpt.isPresent()) {
             User user = userOpt.get();
-            log.info("user = {}", user);
+            emailService.sendUserIdEmail(user);
 
-            return "users/findUserByEmail";
+            return "redirect:/login";
         } else {
-            return "redirect:/";
+            return "redirect:/find/user";
         }
     }
 }
