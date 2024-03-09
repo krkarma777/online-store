baf0e15511f5a033dc022dc4a2be8f2d4ecf426e
krkarma777 <krkarma777@gmail.com>
2024-03-09 23:19:44 +0900

    Resolve JSON serialization issue #24 by introducing ShippingAddressDTO
    - Implement ShippingAddressDTO to avoid infinite recursion during JSON serialization by explicitly defining the data structure for the API response.
    - Use ShippingAddressDTO in controller responses to ensure proper handling of entity relationships and to prevent `StackOverflowError` caused by cyclic references in entities.
    - Add constructors in ShippingAddressDTO to facilitate easy conversion from ShippingAddress entities and to initialize DTOs for API responses.

--- src/main/java/com/bulkpurchase/domain/dto/user/ShippingAddressDTO.java
+++ src/main/java/com/bulkpurchase/domain/dto/user/ShippingAddressDTO.java
@@ -1,5 +1,6 @@
 package com.bulkpurchase.domain.dto.user;
 
+import com.bulkpurchase.domain.entity.user.ShippingAddress;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,4 +19,16 @@ public class ShippingAddressDTO {
     private String zipCode;
 
     private String phoneNumber;
+
+    public ShippingAddressDTO(ShippingAddress shippingAddress) {
+        this.id = shippingAddress.getId();
+        this.recipientName = shippingAddress.getRecipientName();
+        this.address = shippingAddress.getAddress();
+        this.detailAddress = shippingAddress.getDetailAddress();
+        this.zipCode = shippingAddress.getZipCode();
+        this.phoneNumber = shippingAddress.getPhoneNumber();
+    }
+
+    public ShippingAddressDTO() {
+    }
 }
