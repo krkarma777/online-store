163a3aee8a5cb1663802a4430357d3b2c836b2ac
krkarma777 <krkarma777@gmail.com>
2024-03-09 14:46:59 +0900

    test: Add unit test for Inquiry creation in InquiryController
    This commit introduces a new unit test `whenPostInquiry_thenCreateInquiry` in `InquiryControllerTest`. The test ensures that the Inquiry creation endpoint correctly handles a valid InquiryCreateRequestDTO and responds with a status of 201 (Created). It utilizes `@WithMockUser` annotation to simulate an authenticated user with the role of "자영업자" making the request, validating the controller's behavior under authenticated conditions. This enhancement to our test suite aims to improve code reliability by verifying the functionality of inquiry submission through the API.

---
+++ src/test/java/com/bulkpurchase/web/controller/api/InquiryControllerTest.java
@@ -0,0 +1,39 @@
+package com.bulkpurchase.web.controller.api;
+
+import com.bulkpurchase.domain.dto.inquiry.InquiryCreateRequestDTO;
+import com.bulkpurchase.domain.enums.inquiry.InquiryType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InquiryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username="zxczxc", roles={"자영업자"})
+    public void whenPostInquiry_thenCreateInquiry() throws Exception {
+        InquiryCreateRequestDTO inquiryDTO = new InquiryCreateRequestDTO();
+        inquiryDTO.setTitle("Test Inquiry");
+        inquiryDTO.setInquiryContent("This is a test content");
+        inquiryDTO.setInquiryType(InquiryType.GENERAL);
+
+        mockMvc.perform(post("/api/inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(inquiryDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.response").value("문의가 성공적으로 작성되었습니다."));
+    }
+}
