82fd20ab3ee48ea7bcaabcc191abeccb676f45be
krkarma777 <krkarma777@gmail.com>
2024-03-15 00:38:19 +0900

    Add test for listing coupons with descending order in CouponController
    - Implement listCouponsTest to verify listing all coupons in descending order by ID is successful.

--- src/test/java/com/bulkpurchase/web/controller/api/CouponControllerTest.java
+++ src/test/java/com/bulkpurchase/web/controller/api/CouponControllerTest.java
@@ -14,11 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -47,14 +49,12 @@ public class CouponControllerTest {
 
     private User mockUser;
 
-    private Coupon coupon;
+    private final Coupon coupon = new Coupon();;
 
     @BeforeEach
     public void setUp() {
         mockUser = new User();
-        Coupon couponOrigin = new Coupon();
-        couponOrigin.setCouponID(1L);
-        coupon = couponOrigin;
+        coupon.setCouponID(1L);
     }
 
     @Test
@@ -76,13 +76,14 @@ public class CouponControllerTest {
     @Test
     @WithMockUser(username = "zxczxc", roles = "자영업자")
     public void updateCouponTest() throws Exception {
+        // Given
         Long couponId = coupon.getCouponID();
+        CouponUpdateRequestDTO couponUpdateRequestDTO = new CouponUpdateRequestDTO();
 
         when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(mockUser);
         when(couponService.findByIdAndUser(eq(couponId), eq(mockUser))).thenReturn(Optional.of(coupon));
 
-        CouponUpdateRequestDTO couponUpdateRequestDTO = new CouponUpdateRequestDTO();
-
+        // When & Then
         mockMvc.perform(put("/api/coupon/{couponID}", couponId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(couponUpdateRequestDTO)))
@@ -95,11 +96,13 @@ public class CouponControllerTest {
     @Test
     @WithMockUser(username = "zxczxc", roles = "자영업자")
     public void deleteCouponTest() throws Exception {
+        // Given
         Long couponId = coupon.getCouponID();
 
         when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(mockUser);
         when(couponService.findByIdAndUser(couponId, mockUser)).thenReturn(Optional.of(coupon));
 
+        // When & Then
         mockMvc.perform(delete("/api/coupon/{couponID}", couponId))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"message\":\"쿠폰이 정상적으로 삭제되었습니다.\"}"));
@@ -110,9 +113,12 @@ public class CouponControllerTest {
     @Test
     @WithMockUser(username = "testUser")
     public void getCouponTest_Success() throws Exception {
+        // Given
         Long couponID = coupon.getCouponID();
+
         when(couponService.findById(eq(couponID))).thenReturn(Optional.of(coupon));
 
+        // When & Then
         mockMvc.perform(get("/api/coupon/{couponID}", couponID))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(coupon)));
@@ -121,11 +127,33 @@ public class CouponControllerTest {
     @Test
     @WithMockUser(username = "testUser")
     public void getCouponTest_NotFound() throws Exception {
+        // Given
         Long couponId = 2L;
+
         when(couponService.findById(eq(couponId))).thenReturn(Optional.empty());
 
+        // When & Then
         mockMvc.perform(get("/api/coupon/{couponID}", couponId))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json("{\"message\":\"쿠폰이 존재하지 않습니다.\"}"));
+    }
+
+    @Test
+    @WithMockUser(username = "testUser")
+    public void listCouponsTest() throws Exception {
+        // Given
+        List<Coupon> expectedCoupons = List.of(
+                new Coupon(),
+                new Coupon()
+        );
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        when(couponService.findAll(sort)).thenReturn(expectedCoupons);
+
+        // When & Then
+        mockMvc.perform(get("/api/coupon/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedCoupons)));
+
+        verify(couponService).findAll(sort);
     }
 }
