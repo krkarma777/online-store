019047e738ea614aebe63142663ddd9677557cd4
krkarma777 <krkarma777@gmail.com>
2024-03-17 15:41:56 +0900

    refactor(api): Redesign OrderAPIController to enhance RESTful design and integrate CartService
    - Refactored existing API endpoints in OrderAPIController for better adherence to RESTful principles.
    - Integrated CartService to facilitate retrieving orders directly from a user's cart, adding a new endpoint `/cart/{cartID}` to handle order form requests based on cart items.
    - Included CartItemOrderResponseDTO in the order form endpoint to return detailed cart item information suitable for order processing.
    - Ensured API security by validating user access to the cart, enhancing the security and integrity of the order processing flow.
    
    This commit streamlines the order creation process by allowing direct interaction with the cart, improving the user experience and backend efficiency.

--- src/main/java/com/bulkpurchase/web/controller/api/OrderAPIController.java
+++ src/main/java/com/bulkpurchase/web/controller/api/OrderAPIController.java
@@ -1,14 +1,14 @@
 package com.bulkpurchase.web.controller.api;
 
+import com.bulkpurchase.domain.dto.cart.CartItemOrderResponseDTO;
 import com.bulkpurchase.domain.dto.order.OrderDetailResponseDTO;
 import com.bulkpurchase.domain.dto.order.OrderResponseDTO;
 import com.bulkpurchase.domain.dto.order.OrderViewDTO;
 import com.bulkpurchase.domain.dto.order.PaymentResponseDTO;
-import com.bulkpurchase.domain.dto.orderdetail.OrderDetailViewDTO;
+import com.bulkpurchase.domain.entity.cart.Cart;
 import com.bulkpurchase.domain.entity.order.Order;
-import com.bulkpurchase.domain.entity.order.OrderDetail;
-import com.bulkpurchase.domain.entity.order.Payment;
 import com.bulkpurchase.domain.entity.user.User;
+import com.bulkpurchase.domain.service.cart.CartService;
 import com.bulkpurchase.domain.service.order.OrderDetailService;
 import com.bulkpurchase.domain.service.order.OrderService;
 import com.bulkpurchase.domain.service.order.PaymentService;
@@ -16,7 +16,6 @@ import com.bulkpurchase.domain.validator.user.UserAuthValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -33,6 +32,7 @@ public class OrderAPIController {
     private final OrderDetailService orderDetailService;
     private final PaymentService paymentService;
     private final UserAuthValidator userAuthValidator;
+    private final CartService cartService;
 
     @GetMapping("/list")
     public ResponseEntity<?> orderList(Principal principal) {
@@ -53,5 +53,25 @@ public class OrderAPIController {
         List<OrderDetailResponseDTO> detailResponseDTOS = orderDetailService.findByOrder(order).stream().map(OrderDetailResponseDTO::new).toList();
 
         return ResponseEntity.ok(Map.of("orderDetailList", detailResponseDTOS,"order", orderResponseDTO,"payment", paymentResponseDTO));
+    }
+
+    @GetMapping("/cart/{cartID}")
+    public ResponseEntity<?> orderForm(@PathVariable(value = "cartID") Long cartID, @RequestParam("itemIDs") List<Long> itemIDs, Principal principal) {
+        Cart cart = cartService.findById(cartID).orElse(null);
+        if (cart == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", "장바구니가 존재하지 않습니다."));
+        }
+
+        User user = userAuthValidator.getCurrentUser(principal);
+        if (!cart.getUser().equals(user)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "잘못된 요청입니다."));
+        }
+
+        List<CartItemOrderResponseDTO> orderResponseDTOS = cart.getItems().stream()
+                .filter(cartItem -> itemIDs.contains(cartItem.getCartItemID()))
+                .map(CartItemOrderResponseDTO::new)
+                .toList();
+
+        return ResponseEntity.ok(orderResponseDTOS);
     }
 }
