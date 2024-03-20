67ed75989c048a65ea6c7e262868caf95b7b45a0
krkarma777 <krkarma777@gmail.com>
2024-03-20 13:55:05 +0900

    Refactor ReviewResponseDTO to format creationDate
    - Change creationDate field type from LocalDateTime to String in ReviewResponseDTO to include formatted date.
    - Apply DateTimeFormatter to format creationDate as "yyyy/MM/dd HH:mm" directly in the constructor.
    - This modification ensures that the date format is consistently represented across different parts of the application, facilitating easier data handling and presentation on the front-end.

--- src/main/java/com/bulkpurchase/domain/dto/review/ReviewResponseDTO.java
+++ src/main/java/com/bulkpurchase/domain/dto/review/ReviewResponseDTO.java
@@ -1,11 +1,10 @@
 package com.bulkpurchase.domain.dto.review;
 
-import com.bulkpurchase.domain.entity.order.OrderDetail;
 import com.bulkpurchase.domain.entity.review.Review;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Getter
@@ -17,7 +16,7 @@ public class ReviewResponseDTO {
     private String productImg;
     private String content;
     private Integer rating;
-    private LocalDateTime creationDate;
+    private String creationDate;
     private List<String> imageUrls;
     private Long orderId;
     private String realName;
@@ -28,7 +27,7 @@ public class ReviewResponseDTO {
         this.productImg = review.getProduct().getImageUrls().get(0);
         this.content = review.getContent();
         this.rating = review.getRating();
-        this.creationDate = review.getCreationDate();
+        this.creationDate =  DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm").format(review.getCreationDate());
         this.imageUrls = review.getImageUrls();
         this.orderId = review.getOrderDetail().getOrder().getOrderID();
         this.realName = review.getUser().getRealName();
