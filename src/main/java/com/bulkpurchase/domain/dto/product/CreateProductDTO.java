70562c4fbde077351cbc2744ed61c5e6a76a83ca
krkarma777 <krkarma777@gmail.com>
2024-03-23 13:02:57 +0900

    Add CreateProductDTO for product creation API

---
+++ src/main/java/com/bulkpurchase/domain/dto/product/CreateProductDTO.java
@@ -0,0 +1,28 @@
+package com.bulkpurchase.domain.dto.product;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CreateProductDTO {
+    @NotEmpty
+    private String productName;
+
+    @NotEmpty
+    private String description;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Double price;
+
+    @NotNull
+    private Integer stock;
+
+    private List<String> imageUrls;
+}
