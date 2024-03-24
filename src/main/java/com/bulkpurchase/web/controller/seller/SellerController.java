cf02a6ff9fd217823fd3cc99703e5e6b98592e94
krkarma777 <krkarma777@gmail.com>
2024-03-25 00:21:15 +0900

    Refactor SellerReviewManageController and related view for dynamic content loading
    - Refactor SellerReviewManageController to SellerController and adjust its logic to pass sellerID instead of reviews list directly to the model. This change facilitates the front-end to fetch reviews dynamically via an API call.
    - Update the corresponding Thymeleaf view to remove static content rendering of reviews. Integrate Axios to fetch reviews data dynamically from the server and implement pagination functionality. This improves the page's responsiveness and scalability by loading reviews content on demand.
    - Add Axios script import in the HTML header for making HTTP requests.

--- src/main/java/com/bulkpurchase/web/controller/seller/SellerController.java
+++ src/main/java/com/bulkpurchase/web/controller/seller/SellerController.java
@@ -1,8 +1,5 @@
 package com.bulkpurchase.web.controller.seller;
 
-import com.bulkpurchase.domain.dto.review.ReviewDetailDTO;
-import com.bulkpurchase.domain.entity.user.User;
-import com.bulkpurchase.domain.service.review.ReviewService;
 import com.bulkpurchase.domain.validator.user.UserAuthValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,20 +7,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class SellerReviewManageController {
+public class SellerController {
 
-    private final ReviewService reviewService;
     private final UserAuthValidator userAuthValidator;
 
     @GetMapping("/seller/reviews")
     public String reviewManage(Principal principal, Model model) {
-        User user = userAuthValidator.getCurrentUser(principal);
-        List<ReviewDetailDTO> reviews = reviewService.findAllReviewDetailsWithFeedbackCountsBySeller(user.getUserID());
-        model.addAttribute("reviews", reviews);
+        Long sellerID = userAuthValidator.getCurrentUser(principal).getUserID();
+        model.addAttribute("sellerID", sellerID);
         return "/seller/productManage/reviews";
     }
 }
