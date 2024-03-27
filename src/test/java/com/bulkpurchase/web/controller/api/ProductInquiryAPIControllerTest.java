b69f9b83d5de8f429e43cd3a2187c0b3a17fa3bf
krkarma777 <krkarma777@gmail.com>
2024-03-27 14:34:21 +0900

    Add new test cases for ProductInquiry API's update functionality
    - Implemented new test cases to cover update scenarios in ProductInquiryAPIControllerTest. This includes handling successful updates, updates for non-existent inquiries, unauthorized update attempts, updates for inquiries with replies, and updates with invalid request bodies.
    - Enhanced test coverage for edge cases and improved code reliability by ensuring update functionality behaves as expected under various conditions.
    - Refactored setup method to accommodate additional test cases, ensuring each test operates with a clean state.

--- src/test/java/com/bulkpurchase/web/controller/api/ProductInquiryAPIControllerTest.java
+++ src/test/java/com/bulkpurchase/web/controller/api/ProductInquiryAPIControllerTest.java
@@ -1,13 +1,13 @@
 package com.bulkpurchase.web.controller.api;
 
 import com.bulkpurchase.domain.dto.productInquiry.ProductInquiryCreateRequestDTO;
+import com.bulkpurchase.domain.dto.productInquiry.ProductInquiryUpdateRequestDTO;
 import com.bulkpurchase.domain.entity.product.Product;
 import com.bulkpurchase.domain.entity.product.ProductInquiry;
 import com.bulkpurchase.domain.entity.user.User;
 import com.bulkpurchase.domain.service.product.ProductInquiryService;
 import com.bulkpurchase.domain.service.product.ProductService;
 import com.bulkpurchase.domain.validator.user.UserAuthValidator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,10 +21,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,38 +49,42 @@ class ProductInquiryAPIControllerTest {
     @MockBean
     private ProductService productService;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private User user;
-    private ProductInquiryCreateRequestDTO requestDTO;
+    private ProductInquiryCreateRequestDTO createRequestDTO;
+    private ProductInquiryUpdateRequestDTO updateRequestDTO;
     private Product product;
     private ProductInquiry productInquiry;
 
     @BeforeEach
     void setUp() {
         user = new User();
-        requestDTO = new ProductInquiryCreateRequestDTO();
-        requestDTO.setProductID(1L);
-        requestDTO.setInquiryContent("test inquiry");
+        createRequestDTO = new ProductInquiryCreateRequestDTO();
+        createRequestDTO.setProductID(1L);
+        createRequestDTO.setInquiryContent("test inquiry");
 
         product = new Product();
         product.setProductID(1L);
 
-        productInquiry = new ProductInquiry(requestDTO, product, user);
+        productInquiry = new ProductInquiry(createRequestDTO, product, user);
         productInquiry.setInquiryID(1L);
 
-        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
-        given(productInquiryService.save(any(ProductInquiry.class))).willReturn(productInquiry);
+        updateRequestDTO = new ProductInquiryUpdateRequestDTO();
+        updateRequestDTO.setInquiryID(1L);
+        updateRequestDTO.setInquiryContent("updated inquiry content");
     }
     @Test
     @WithMockUser
     public void testCreateSuccessfully() throws Exception {
-
         given(productService.findById(1L)).willReturn(Optional.of(product));
+        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
+        given(productInquiryService.save(any(ProductInquiry.class))).willReturn(productInquiry);
 
         mockMvc.perform(post("/api/product-inquiry")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDTO)))
+                        .content(objectMapper.writeValueAsString(createRequestDTO)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.inquiryID").value(1L))
                 .andExpect(jsonPath("$.message").value("문의가 정상적으로 등록되었습니다."));
@@ -87,11 +93,79 @@ class ProductInquiryAPIControllerTest {
     @Test
     @WithMockUser
     public void testCreateForNonexistentProduct() throws Exception {
+        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
+        given(productInquiryService.save(any(ProductInquiry.class))).willReturn(productInquiry);
 
         mockMvc.perform(post("/api/product-inquiry")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(requestDTO)))
+                        .content(objectMapper.writeValueAsString(createRequestDTO)))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("상품이 존재하지 않습니다."));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateSuccessfully() throws Exception {
+        given(productInquiryService.findById(1L)).willReturn(Optional.of(productInquiry));
+        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
+
+        mockMvc.perform(patch("/api/product-inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.inquiryID").value(1L))
+                .andExpect(jsonPath("$.message").value("문의 수정이 완료되었습니다."));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateForNonexistentInquiry() throws Exception {
+        given(productInquiryService.findById(1L)).willReturn(Optional.empty());
+
+        mockMvc.perform(patch("/api/product-inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequestDTO)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("문의가 존재하지 않습니다."));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateForUnauthorizedUser() throws Exception {
+        productInquiry.setUser(new User());
+
+        given(productInquiryService.findById(1L)).willReturn(Optional.of(productInquiry));
+        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
+
+        mockMvc.perform(patch("/api/product-inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequestDTO)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.message").value("글을 수정할 권한이 없습니다."));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateForRepliedInquiry() throws Exception {
+        productInquiry.setReplyDate(new Date());
+
+        given(productInquiryService.findById(1L)).willReturn(Optional.of(productInquiry));
+        given(userAuthValidator.getCurrentUser(any(Principal.class))).willReturn(user);
+
+        mockMvc.perform(patch("/api/product-inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequestDTO)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("답변이 달린 글은 수정할 수 없습니다."));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateWithInvalidRequest() throws Exception {
+        mockMvc.perform(patch("/api/product-inquiry")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."));
     }
 }
