0b8f47047dbaa75f6efe273c2f1845edfc97c911
krkarma777 <krkarma777@gmail.com>
2024-04-09 21:26:29 +0900

    Add Order Management APIs for Seller Interface
    - Implement `manageForm` API to fetch order details for sellers with pagination.
    - Add `orderStatusChange` API to allow sellers to update the status of an order detail.
    - Create `orderDetailInfoForSeller` API to provide detailed information about an order to the seller, including order details, payment, and order response data.
    - Include exception handling for cases where order details are not found, improving error management and response.
    - Utilize `UserAuthValidator` to ensure that only authorized sellers can access and modify order details.

---
+++ src/main/java/com/bulkpurchase/web/controller/api/OrderManagementAPIController.java
@@ -0,0 +1,80 @@
+package com.bulkpurchase.web.controller.api;
+
+import com.bulkpurchase.domain.dto.order.OrderDetailResponseDTO;
+import com.bulkpurchase.domain.dto.order.OrderResponseDTO;
+import com.bulkpurchase.domain.dto.order.PaymentResponseDTO;
+import com.bulkpurchase.domain.entity.order.OrderDetail;
+import com.bulkpurchase.domain.entity.user.User;
+import com.bulkpurchase.domain.enums.OrderStatus;
+import com.bulkpurchase.domain.service.order.OrderDetailService;
+import com.bulkpurchase.domain.service.order.PaymentService;
+import com.bulkpurchase.domain.validator.user.UserAuthValidator;
+import com.bulkpurchase.web.service.order.OrderStatusUpdateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order-management")
+public class OrderManagementAPIController {
+
+    private final OrderDetailService orderDetailService;
+    private final OrderStatusUpdateService orderStatusUpdateService;
+    private final PaymentService paymentService;
+    private final UserAuthValidator userAuthValidator;
+
+    @GetMapping
+    public ResponseEntity<?> manageForm(Principal principal, @RequestParam(value = "page", defaultValue = "1") Integer page) {
+        User user = userAuthValidator.getCurrentUser(principal);
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "inquiryID");
+
+        PageRequest pageRequest = PageRequest.of(page - 1, 10, sort);
+
+        // 조회된 데이터와 함께 총 페이지 수를 맵으로 구성하여 반환합니다.
+        // 'orderDetails' 키에는 OrderDetail 객체들을 OrderDetailManagementResponseDTO 객체로 변환한 리스트를 할당합니다.
+        // 'totalPages' 키에는 조회 결과의 총 페이지 수를 할당합니다.
+        Map<String, Object> responseData  = orderDetailService.findBySeller(user, pageRequest);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @PatchMapping("/{orderDetailID}")
+    public ResponseEntity<?> orderStatusChange(@PathVariable("orderDetailID") Long orderDetailID, Principal principal, @RequestParam("orderStatus") OrderStatus status) {
+        User user = userAuthValidator.getCurrentUser(principal);
+        OrderDetail orderDetail = getOrderDetail(orderDetailID);
+
+        userAuthValidator.validateUserAccessOrderDetail(user, orderDetail);
+        orderStatusUpdateService.updateOrderStatus(orderDetail, status);
+
+        return ResponseEntity.ok("/seller/orders");
+    }
+
+    @GetMapping("/detail/{orderDetailID}")
+    public ResponseEntity<?> orderDetailInfoForSeller(@PathVariable("orderDetailID") Long orderDetailID) {
+        OrderDetail orderDetail = getOrderDetail(orderDetailID);
+
+        OrderDetailResponseDTO orderDetailResponseDTO = new OrderDetailResponseDTO(orderDetail);
+        OrderResponseDTO orderResponseDTO = new OrderResponseDTO(orderDetail.getOrder());
+        PaymentResponseDTO paymentResponseDTO = new PaymentResponseDTO(paymentService.findByOrder(orderDetail.getOrder()));
+
+        return ResponseEntity.ok(Map.of(
+                "orderDetail", orderDetailResponseDTO,
+                "order", orderResponseDTO,
+                "payment", paymentResponseDTO
+        ));
+    }
+
+    private OrderDetail getOrderDetail(Long orderDetailID) {
+        return orderDetailService.findByID(orderDetailID)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "주문 정보가 존재하지 않습니다."));
+    }
+}
