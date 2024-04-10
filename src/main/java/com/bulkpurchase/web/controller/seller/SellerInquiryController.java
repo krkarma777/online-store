85f97e9c77a252972157cdd7a02d84e047ca916a
krkarma777 <krkarma777@gmail.com>
2024-04-11 01:17:01 +0900

    Refactor SellerInquiryController for Simplified Inquiry Handling
    This commit simplifies the SellerInquiryController by removing unnecessary dependencies and streamlining the inquiry listing process. By focusing on a more simplified approach, the controller now promotes a clearer structure for managing seller inquiries without direct dependency on user authentication and service layers for listing inquiries. The goal is to leverage client-side handling for a more dynamic and responsive user interface.

--- src/main/java/com/bulkpurchase/web/controller/seller/SellerInquiryController.java
+++ src/main/java/com/bulkpurchase/web/controller/seller/SellerInquiryController.java
@@ -1,9 +1,5 @@
 package com.bulkpurchase.web.controller.seller;
 
-import com.bulkpurchase.domain.entity.Inquiry;
-import com.bulkpurchase.domain.entity.user.User;
-import com.bulkpurchase.domain.service.InquiryService;
-import com.bulkpurchase.domain.validator.user.UserAuthValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
-import java.util.List;
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/seller/inquiries")
 @RequiredArgsConstructor
 public class SellerInquiryController {
-
-    private final UserAuthValidator userAuthValidator;
-    private final InquiryService inquiryService;
 
     @GetMapping("/{inquiryID}")
     public String inquiry(Model model, @PathVariable("inquiryID") Long inquiryID) {
@@ -29,10 +18,7 @@ public class SellerInquiryController {
         return "seller/seller-mypage/customer_center_inquiry";
     }
     @GetMapping("/list")
-    public String inquiries(Model model, Principal principal) {
-        User user = userAuthValidator.getCurrentUser(principal);
-        List<Inquiry> inquiries = inquiryService.findByUser(user);
-        model.addAttribute("inquiries", inquiries);
+    public String inquiries() {
         return "seller/seller-mypage/customer_center_inquiries";
     }
     @GetMapping("/new")
