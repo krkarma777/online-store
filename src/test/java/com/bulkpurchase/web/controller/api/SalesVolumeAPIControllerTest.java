9d08f5bed979ac66e613d0b0154714d9ec0f9cbe
krkarma777 <krkarma777@gmail.com>
2024-04-10 19:23:53 +0900

    Add New Tests for Sales Data with @WithMockUser
    - Added tests for fetching sales data over the last year and the last three years
    - Utilized @WithMockUser to simulate authenticated user scenarios in tests
    - Adjusted date formatting to "yyyy-MM-dd" for consistent date handling
    - Ensured the setup of a mock user with ROLE_판매자 to test role-based access control

--- src/test/java/com/bulkpurchase/web/controller/api/SalesVolumeAPIControllerTest.java
+++ src/test/java/com/bulkpurchase/web/controller/api/SalesVolumeAPIControllerTest.java
@@ -2,6 +2,7 @@ package com.bulkpurchase.web.controller.api;
 
 import com.bulkpurchase.domain.dto.seller.SalesDataDTO;
 import com.bulkpurchase.domain.entity.user.User;
+import com.bulkpurchase.domain.enums.UserRole;
 import com.bulkpurchase.domain.service.order.OrderService;
 import com.bulkpurchase.domain.validator.user.UserAuthValidator;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -58,23 +60,26 @@ class SalesVolumeAPIControllerTest {
 
         user = new User();
         user.setUserID(1L);
+        user.setRole(UserRole.ROLE_판매자);
+        System.out.println("user = " + user);
+        when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(user);
     }
 
     @Test
+    @WithMockUser
     public void testFindSalesDataForLast30Days() throws Exception {
         Principal mockPrincipal = mock(Principal.class);
         when(mockPrincipal.getName()).thenReturn("testUser");
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         List<SalesDataDTO> salesDataDTOList = new ArrayList<>();
         // 예시 데이터 추가
         salesDataDTOList.add(new SalesDataDTO(formatter.format(startDate),  new BigDecimal("1000")));
         salesDataDTOList.add(new SalesDataDTO(formatter.format(startDate.plusDays(1)),  new BigDecimal("1500")));
 
-        when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(user);
         when(orderService.calculateSalesLast30DaysBySeller(eq(user.getUserID()), eq(startDate), eq(endDate))).thenReturn(salesDataDTOList);
 
-        mockMvc.perform(get("/api/sales-volume")
+        mockMvc.perform(get("/api/sales-volume/last-30-days")
                         .principal(mockPrincipal)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -83,4 +88,53 @@ class SalesVolumeAPIControllerTest {
         verify(orderService, times(1)).calculateSalesLast30DaysBySeller(eq(user.getUserID()), eq(startDate), eq(endDate));
     }
 
+    // 지난 12개월 판매 데이터 테스트
+    @Test
+    @WithMockUser
+    public void testFindSalesDataOverLastYear() throws Exception {
+        Principal mockPrincipal = mock(Principal.class);
+        when(mockPrincipal.getName()).thenReturn("testUser");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        List<SalesDataDTO> salesDataDTOList = new ArrayList<>();
+        // 예시 데이터 추가
+        salesDataDTOList.add(new SalesDataDTO(formatter.format(startDate.minusMonths(11)), new BigDecimal("2000")));
+        salesDataDTOList.add(new SalesDataDTO(formatter.format(startDate.minusMonths(10)), new BigDecimal("2500")));
+
+        when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(user);
+        when(orderService.calculateSalesLast12MonthsBySeller(eq(user.getUserID()))).thenReturn(salesDataDTOList);
+
+        mockMvc.perform(get("/api/sales-volume/last-year")
+                        .principal(mockPrincipal)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(salesDataDTOList)));
+
+        verify(orderService, times(1)).calculateSalesLast12MonthsBySeller(eq(user.getUserID()));
+    }
+
+    // 최근 3년 판매 데이터 테스트
+    @Test
+    @WithMockUser
+    public void testFindSalesDataOverLastThreeYears() throws Exception {
+        Principal mockPrincipal = mock(Principal.class);
+        when(mockPrincipal.getName()).thenReturn("testUser");
+
+        List<SalesDataDTO> salesDataDTOList = new ArrayList<>();
+        // 예시 데이터 추가
+        salesDataDTOList.add(new SalesDataDTO("2019", new BigDecimal("12000")));
+        salesDataDTOList.add(new SalesDataDTO("2020", new BigDecimal("15000")));
+        salesDataDTOList.add(new SalesDataDTO("2021", new BigDecimal("18000")));
+
+        when(userAuthValidator.getCurrentUser(any(Principal.class))).thenReturn(user);
+        when(orderService.calculateSalesLast3YearsBySeller(eq(user.getUserID()))).thenReturn(salesDataDTOList);
+
+        mockMvc.perform(get("/api/sales-volume/last-three-years")
+                        .principal(mockPrincipal)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(salesDataDTOList)));
+
+        verify(orderService, times(1)).calculateSalesLast3YearsBySeller(eq(user.getUserID()));
+    }
 }
